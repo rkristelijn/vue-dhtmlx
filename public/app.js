6e083ae3bfb29325ec28079d5c56d8a6f1f31854
Remi Kristelijn <rkristelijn@gmail.com>
2018-06-14 11:15:44 +0200

    fixed both vue and dhx related

--- public/app.js
+++ public/app.js
@@ -1,22 +1,32 @@
 let vueApp = new Vue({
   el: '#app',
   data: {
-    message: 'hello world'
-  }
-});
-
-window.addEventListener('load', () => {
-  console.log('loading dhtmlx...');
-  mainSidebar = new dhtmlXSideBar({
-    parent: document.body,
-    icons_path: "imgs/sidebar/",
-    width: 180,
-    template: "tiles",
+    message: 'hello world',
     items: [
       { id: "contacts", text: "Contacts", icon: "contacts.png" },
       { id: "projects", text: "Projects", icon: "projects.png" },
       { id: "events", text: "Events", icon: "events.png" },
       { id: "settings", text: "Settings", icon: "settings.png" }
     ]
-  });
+  },
+  methods: {
+    initDhx: function () {
+      this.message = 'hello init';
+      let mainSidebar = new dhtmlXSideBar({
+        parent: 'app',
+        icons_path: "imgs/sidebar/",
+        width: 180,
+        template: "tiles"
+      });
+      console.log(this.items);
+      for(item of this.items) {
+        mainSidebar.addItem(item);
+      }
+      console.log(mainSidebar);
+    }
+  },
+  mounted: function () {
+    console.log('loading dhtmlx...', this.message);
+    this.initDhx();
+  }
 });
