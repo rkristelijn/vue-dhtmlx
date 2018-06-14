642814f5e492e52213356c7a222ced311601b3d8
Remi Kristelijn <rkristelijn@gmail.com>
2018-06-14 14:22:27 +0200

    first integration but not happy about it

--- public/app.js
+++ public/app.js
@@ -1,32 +1,63 @@
 let vueApp = new Vue({
   el: '#app',
   data: {
-    message: 'hello world',
-    items: [
-      { id: "contacts", text: "Contacts", icon: "contacts.png" },
-      { id: "projects", text: "Projects", icon: "projects.png" },
-      { id: "events", text: "Events", icon: "events.png" },
-      { id: "settings", text: "Settings", icon: "settings.png" }
+    settings: {
+      iconsPath: 'imgs/'
+    },
+    sideBarItems: [
+      { id: 'contacts', text: 'Contacts', icon: 'contacts.png' },
+      { id: 'projects', text: 'Projects', icon: 'projects.png' },
+      { id: 'events', text: 'Events', icon: 'events.png' },
+      { id: 'settings', text: 'Settings', icon: 'settings.png' }
+    ],
+    toolBarItems: [
+      { type: 'text', id: 'title', text: '&nbsp;' },
+      { type: 'spacer' },
+      { type: 'button', id: 'add', img: 'add.png' },
+      { type: 'button', id: 'del', img: 'del.png' }
     ]
   },
   methods: {
     initDhx: function () {
-      this.message = 'hello init';
+      let that = this;
       let mainSidebar = new dhtmlXSideBar({
         parent: 'app',
-        icons_path: "imgs/sidebar/",
+        icons_path: `${this.settings.iconsPath}sidebar/`,
         width: 180,
-        template: "tiles"
+        template: 'tiles',
+        items: this.sideBarItems
       });
-      console.log(this.items);
-      for(item of this.items) {
-        mainSidebar.addItem(item);
-      }
-      console.log(mainSidebar);
+
+      let mainToolbar = mainSidebar.attachToolbar({
+        icons_size: 32,
+        icons_path: `${this.settings.iconsPath}toolbar/`,
+        items: this.toolBarItems
+      });
+
+      mainSidebar.attachEvent('onSelect', function (id) {
+        window.dhx4.callEvent('onSidebarSelect', [id, this.cells(id)]);
+        myForm = mainSidebar.cells(id).attachForm([
+          { type: 'input', name: 'label', label: 'Label' },
+          {
+            type: 'combo', label: 'Icon', name: 'icon', options: [
+              { text: 'contacts.png' },
+              { text: 'projects.png', selected: true },
+              { text: 'events.png' },
+              { text: 'settings.png' }
+            ]
+          },
+          { type: 'button', value: 'Add' }
+        ]);
+        myForm.attachEvent('onButtonClick', function (id) {
+          let iconName = myForm.getCombo('icon').getSelectedText();
+          mainSidebar.addItem({ id: myForm.getItemValue('label'), text: myForm.getItemValue('label'), icon: iconName });
+          //that.sideBarItems.push({id: myForm.getItemValue('label'), text: myForm.getItemValue('label'), icon: iconName});
+          //console.log(that.sideBarItems);
+        });
+      });
     }
   },
   mounted: function () {
-    console.log('loading dhtmlx...', this.message);
     this.initDhx();
   }
 });
